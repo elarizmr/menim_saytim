9be9ba956eba77ccc5244d074c3e84f3bb3eab17
Elariz Recebov <elarizrecebov@syn-172-090-000-136.res.spectrum.com>
2026-02-08 16:57:34 +0400

    slaam

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__892bc1ad._.js",
-  "static/chunks/node_modules_c1f2323c._.js",
-  "static/chunks/[root-of-the-server]__36b083e1._.css"
-],
-    source: "dynamic"
-});
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,21 +1,27 @@
-import HomeBanners from './home/HomeBanners';
-import ShopPart from './home/ShopPart';
-import GameCategories from './home/GameCategories';
-import Partners from './home/Partners';
-import ShopPartBottom from './home/ShopPartBottom';
-import Reviews from './home/Reviews';
-import BlogPart from './home/BlogPart';
+// import HomeBanners from './home/HomeBanners';
+// import ShopPart from './home/ShopPart';
+// import GameCategories from './home/GameCategories';
+// import Partners from './home/Partners';
+// import ShopPartBottom from './home/ShopPartBottom';
+// import Reviews from './home/Reviews';
+// import BlogPart from './home/BlogPart';
 
-export default function Home() {
-  return (
-    <div>
-      <HomeBanners />
-      <ShopPart />
-      <GameCategories />
-      <Partners />
-      <ShopPartBottom />
-      <Reviews />
-      <BlogPart />
-    </div>
-  );
+// export default function Home() {
+//   return (
+//     <div>
+//       <HomeBanners />
+//       <ShopPart />
+//       <GameCategories />
+//       <Partners />
+//       <ShopPartBottom />
+//       <Reviews />
+//       <BlogPart />
+//     </div>
+//   );
+// }
+
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/home");
 }
